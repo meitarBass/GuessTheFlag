d9d2f1cecbd93cc6d39ee7f05af66f3ad202ef64
meitarbasson <meitarbass@gmail.com>
2022-06-29 10:22:04 +0300

    Initial Commit

---
+++ Guess The Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess The Flag
+//
+//  Created by Meitar Basson on 29/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
